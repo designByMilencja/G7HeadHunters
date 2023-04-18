9445799dc63b1d080b53d11b95c93a716654e204
Milena Pieńkosz <designbymilencja@gmail.com>
2023-04-18 18:20:31 +0100

    added configuration files

--- index.ts
+++ index.ts
@@ -3,23 +3,29 @@ import 'express-async-errors';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
 import helmet from "helmet";
+import bodyParser from 'body-parser';
+import dotenv from "dotenv";
+// import bcrypt from "bcrypt";
+// import jwt from "jsonwebtoken";
+// import nodemailer from "nodemailer";
+// import {v4 as uuid} from "uuid";
 import {handleError} from "./utils/handleError";
 import {homeRouter} from "./routers/home.router";
 
-
+dotenv.config();
 const app = express();
-app.use(helmet());
 app.use(json());
+app.use(bodyParser.urlencoded({extended: true}));
+// const secretKey = process.env.SECRET_KEY;
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: process.env.CORS_ORIGIN,
 }));
+app.use(helmet());
 
-
-const limiter = rateLimit({
+app.use(rateLimit({
     windowMs: 10 * 60 * 1000,
     max: 10000,
-});
-app.use(limiter);
+}));
 app.use('/', homeRouter);
 
 app.use(handleError);
