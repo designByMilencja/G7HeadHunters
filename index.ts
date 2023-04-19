83ecf430b36c0adb74cc450f69393a17d0931197
Milena Pieńkosz <designbymilencja@gmail.com>
2023-04-19 13:36:14 +0100

    added prettier configurations and connection to mongodb

--- index.ts
+++ index.ts
@@ -1,35 +1,41 @@
-import express, {json} from 'express';
+import express, { json } from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import rateLimit from 'express-rate-limit';
-import helmet from "helmet";
+import helmet from 'helmet';
 import bodyParser from 'body-parser';
-import dotenv from "dotenv";
+import dotenv from 'dotenv';
+import { db } from './utils/db';
 // import bcrypt from "bcrypt";
 // import jwt from "jsonwebtoken";
 // import nodemailer from "nodemailer";
 // import {v4 as uuid} from "uuid";
-import {handleError} from "./utils/handleError";
-import {homeRouter} from "./routers/home.router";
+import { handleError } from './utils/handleError';
+import { homeRouter } from './routers/home.router';
 
 dotenv.config();
 const app = express();
 app.use(json());
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 // const secretKey = process.env.SECRET_KEY;
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
+  origin: process.env.CORS_ORIGIN,
 }));
 app.use(helmet());
 
 app.use(rateLimit({
-    windowMs: 10 * 60 * 1000,
-    max: 10000,
+  windowMs: 10 * 60 * 1000,
+  max: 10000,
 }));
 app.use('/', homeRouter);
 
 app.use(handleError);
 
-app.listen(3001, '0.0.0.0', () => {
-    console.log('Listening on http://localhost:3001');
-})
+(async () => {
+  try {
+    await db();
+    app.listen(process.env.PORT || 3001, () => console.log('Listening on port 3001'));
+  } catch (err) {
+    console.error(`Connection failed. ${err}`);
+  }
+})();
--- routers/home.router.ts
+++ routers/home.router.ts
@@ -1,6 +1,3 @@
-import {Router, Request, Response} from "express";
+import {Router} from 'express';
+
 export const homeRouter = Router();
-homeRouter
-.get('/', async (req:Request, res:Response): Promise<void> => {
-    res.send({ok:true});
-})
