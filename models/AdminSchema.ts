48f93369fb50455c10d0d5d726fe3d2e758d24dc
s1avo <s.nadolny@hotmail.com>
2023-04-20 21:30:37 +0200

    feat(models): admin schema

---
+++ models/AdminSchema.ts
@@ -0,0 +1,30 @@
+import { Schema, model } from 'mongoose';
+
+const AdminSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      max: 50,
+      unique: true,
+      match: [/^(.+)@(.+)$/, 'Invalid email'],
+    },
+    password: {
+      type: String,
+      required: true,
+      bcrypt: true,
+    },
+    token: {
+      type: String,
+      default: '',
+    },
+    role: {
+      type: String,
+      enum: ['Admin', 'Kursant', 'HR'],
+      default: '',
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const AdminDb = model('Admin', AdminSchema);
