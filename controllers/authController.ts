faee21a7d16779fc1c06951fc2bfa64d897eac6c
s1avo <s.nadolny@hotmail.com>
2023-04-25 17:22:13 +0200

    perf(authentication): optimization of db queries

--- controllers/authController.ts
+++ controllers/authController.ts
@@ -1,10 +1,8 @@
-import { Request, Response } from 'express';
+import { NextFunction, Request, Response } from 'express';
 import jwt from 'jsonwebtoken';
 import bcrypt from 'bcrypt';
 import { hashPwd } from '../utils/hashPwd';
-import { AdminDb } from '../models/AdminSchema';
 import { UserDb } from '../models/UserSchema';
-import { HrDb } from '../models/HrSchema';
 import { ValidationError } from '../utils/handleError';
 
 export const register = async (req: Request, res: Response) => {
@@ -16,54 +14,55 @@ export const register = async (req: Request, res: Response) => {
     role: 'Kursant',
   });
 
-  const savedUser = await newUser.save();
+  const savedUser = await UserDb.createNewUser(newUser, newUser.role);
 
-  res.status(201).send(savedUser); //@TODO zrobić filtrowanie jak zostanie dodany typ UserRepsonse (bez hasła i tokena)
+  res.status(201).send(savedUser); //@TODO zrobić filtrowanie jak zostanie dodany typ UserResponse (bez hasła i tokena)
 };
 
-export const login = async (req: Request, res: Response) => {
-  const { email, password } = req.body;
+export const login = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const { email, password } = req.body;
+    const user = await UserDb.findOne({ email });
+    if (!user) throw new ValidationError('User not found');
 
-  let user = await UserDb.findOne({ email });
-  if (!user) user = await HrDb.findOne({ email });
-  if (!user) user = await AdminDb.findOne({ email });
+    const isMatch = await bcrypt.compare(password, user.password);
+    if (!isMatch) throw new ValidationError('Incorrect email or password');
 
-  if (!user) throw new ValidationError('User not found');
+    const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
+      expiresIn: process.env.EXPIRES_TOKEN,
+    });
 
-  const isMatch = await bcrypt.compare(password, await user.password);
-  if (!isMatch) throw new ValidationError('Wrong password');
+    user.token = token;
+    const savedUser = await user.save();
 
-  const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET, {
-    expiresIn: 60 * 60 * 24,
-  });
-
-  user.token = token;
-  const savedUser = await user.save();
-
-  res
-    .cookie('token', token, {
-      secure: false, //true jeżeli https
-      domain: 'localhost',
-      httpOnly: true,
-    })
-    .send(savedUser); //@TODO zrobić filtrowanie jak zostanie dodany typ UserRepsonse (bez hasła i tokena)
+    res
+      .cookie('token', token, {
+        secure: false, //true jeżeli https
+        domain: 'localhost',
+        httpOnly: true,
+      })
+      .send(savedUser); //@TODO zrobić filtrowanie jak zostanie dodany typ UserResponse (bez hasła i tokena)
+  } catch (err) {
+    next(err);
+  }
 };
 
-export const logout = async (req: Request, res: Response) => {
-  let user = await UserDb.findOne({ token: req.cookies.token });
-  if (!user) user = await HrDb.findOne({ token: req.cookies.token });
-  if (!user) user = await AdminDb.findOne({ token: req.cookies.token });
+export const logout = async (req: Request, res: Response, next: NextFunction) => {
+  try {
+    const user = await UserDb.findOne({ token: req.cookies.token });
+    if (!user) throw new ValidationError('User not found');
 
-  if (!user) throw new ValidationError('User not found');
+    user.token = null;
+    await user.save();
 
-  user.token = null;
-  await user.save();
-
-  res.clearCookie('token', {
-    secure: false, //true jeżeli https
-    domain: 'localhost',
-    httpOnly: true,
-  });
-
-  res.send('Logout successful');
+    res
+      .clearCookie('token', {
+        secure: false, //true jeżeli https
+        domain: 'localhost',
+        httpOnly: true,
+      })
+      .send({ ok: true });
+  } catch (err) {
+    next(err);
+  }
 };
--- routers/auth.router.ts
+++ routers/auth.router.ts
@@ -1,5 +1,6 @@
 import { Router } from 'express';
 import { login, logout, register } from '../controllers/authController';
+import { verifyCookie } from '../middlewares/auth';
 export const authRouter = Router();
 
-authRouter.post('/register', register).post('/login', login).get('/logout', logout);
+authRouter.post('/register', register).post('/login', login).get('/logout', verifyCookie, logout);
