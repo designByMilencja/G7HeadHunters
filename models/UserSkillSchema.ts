28610c4d44908271cdcc859ea576fff0883bd8a9
s1avo <s.nadolny@hotmail.com>
2023-04-25 17:44:07 +0200

    feat(models): skills schema

---
+++ models/UserSkillSchema.ts
@@ -0,0 +1,49 @@
+import { Schema, model } from 'mongoose';
+
+const UserSkillSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      max: 64,
+      unique: true,
+      match: [/^(.+)@(.+)$/, 'Invalid email'],
+      index: true,
+    },
+    courseCompletion: {
+      type: Number,
+      min: 0,
+      max: 5,
+      required: true,
+    },
+    courseEngagement: {
+      type: Number,
+      min: 0,
+      max: 5,
+      required: true,
+    },
+    projectDegree: {
+      type: Number,
+      min: 0,
+      max: 5,
+      required: true,
+    },
+    teamProjectDegree: {
+      type: Number,
+      min: 0,
+      max: 5,
+      required: true,
+    },
+    bonusProjectUrls: {
+      type: [String],
+      required: true,
+    },
+    profile: {
+      type: Schema.Types.ObjectId,
+      ref: 'UserProfile',
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+
+export const UserSkillDb = model('UserSkill', UserSkillSchema);
