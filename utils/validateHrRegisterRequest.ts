91c2c6a060110d6d79a7b1e9e711c86fb39faf3a
s1avo <s.nadolny@hotmail.com>
2023-05-02 09:40:39 +0200

    fix(utils): message for min, max

--- utils/validateHrRegisterRequest.ts
+++ utils/validateHrRegisterRequest.ts
@@ -4,5 +4,9 @@ export const registerHrSchema = yup.object().shape({
   email: yup.string().email('Proszę podać poprawny adres email.').required('Email jest wymagany do rejestracji.'),
   fullName: yup.string().required('Imię i nazwisko jest wymagane do rejestracji.'),
   company: yup.string().required('Nazwę firmy jest wymagane do rejestracji.'),
-  maxReservedStudents: yup.number().min(0).max(999).required('Podaj maksymalną liczbę osób umówionych na rozmowę.'),
+  maxReservedStudents: yup
+    .number()
+    .min(0, 'Minimalna liczba osób 0.')
+    .max(999, 'Maksymalna liczba osób 999.')
+    .required('Podaj maksymalną liczbę osób umówionych na rozmowę.'),
 });
