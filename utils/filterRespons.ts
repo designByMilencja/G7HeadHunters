33bb9963393de11984fe02c29ef6428ce1eccc29
s1avo <s.nadolny@hotmail.com>
2023-05-02 11:43:40 +0200

    fix(filter): add _id to response filters

--- utils/filterRespons.ts
+++ utils/filterRespons.ts
@@ -1,16 +1,16 @@
 import { AdminRespons, HrRespons, IAdmin, IHR, IUser, UserRespons } from '../types';
 
 export const filterUser = (user: IUser): UserRespons => {
-  const { email, role, active, status } = user;
-  return { email, role, active, status };
+  const { _id, email, role, active, status } = user;
+  return { _id, email, role, active, status };
 };
 
 export const filterHr = (hr: IHR): HrRespons => {
-  const { email, role, company, fullName, maxReservedStudents, users } = hr;
-  return { email, role, fullName, company, maxReservedStudents, users };
+  const { _id, email, role, company, fullName, maxReservedStudents, users } = hr;
+  return { _id, email, role, fullName, company, maxReservedStudents, users };
 };
 
 export const filterAdmin = (admin: IAdmin): AdminRespons => {
-  const { email, role } = admin;
-  return { email, role };
+  const { _id, email, role } = admin;
+  return { _id, email, role };
 };
