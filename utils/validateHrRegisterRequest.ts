1b57d14a9f7fa963eef6e0d6885d48f3b6fd0b22
s1avo <s.nadolny@hotmail.com>
2023-05-02 09:03:42 +0200

    fix(utils): removed password, added maxReservedStudents

--- utils/validateHrRegisterRequest.ts
+++ utils/validateHrRegisterRequest.ts
@@ -2,10 +2,7 @@ import * as yup from 'yup';
 
 export const registerHrSchema = yup.object().shape({
   email: yup.string().email('Proszę podać poprawny adres email.').required('Email jest wymagany do rejestracji.'),
-  password: yup
-    .string()
-    .matches(/^.{8,}$/, 'Hasło musi zawierać co najmniej 8 znaków.')
-    .required('Hasło jest wymagane do rejestracji.'),
   fullName: yup.string().required('Imię i nazwisko jest wymagane do rejestracji.'),
   company: yup.string().required('Nazwę firmy jest wymagane do rejestracji.'),
+  maxReservedStudents: yup.number().min(0).max(999).required('Podaj maksymalną liczbę osób umówionych na rozmowę.'),
 });
