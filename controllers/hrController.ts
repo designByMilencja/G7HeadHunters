e888c0fd1ff4ac1916690aeb41830072f686ebfb
s1avo <s.nadolny@hotmail.com>
2023-05-11 20:28:11 +0200

    fix(hrController): set status

--- controllers/hrController.ts
+++ controllers/hrController.ts
@@ -84,15 +84,24 @@ export const setStatus = async (req: Request, res: Response, next: NextFunction)
       throw new ValidationError('Token not match to user');
     }
 
-    const hr = await UserDb.findById(id);
-    hr.users.push(email);
-    hr.save();
-
     const user = await UserDb.findOne({ email });
     if (!user) throw new ValidationError('Users not found');
 
-    user.status = status;
+    user.status.status = status;
     user.save();
+
+    const hr = await UserDb.findById(id);
+    const isUserInArray = hr.users.includes(email);
+
+    if (status === 'W trakcie rozmowy' && !isUserInArray) {
+      hr.users.push(email);
+      hr.save();
+    }
+
+    if (status !== 'W trakcie rozmowy' && isUserInArray) {
+      hr.users = hr.users.filter((e) => e !== email);
+      hr.save();
+    }
 
     res.status(200).send(user);
   } catch (err) {
