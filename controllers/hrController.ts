c7619b7a3089c2bbf4ac5cce05e3c1398735a3bf
s1avo <s.nadolny@hotmail.com>
2023-05-13 10:36:44 +0200

    feat(utils): filter functionality, fix response

--- controllers/hrController.ts
+++ controllers/hrController.ts
@@ -13,7 +13,13 @@ export const availableUsers = async (req: Request, res: Response, next: NextFunc
     const users = await UserSkillDb.find({ email: { $in: availableUsers } }).populate('profile');
     if (!users) throw new ValidationError('Users not found');
 
-    const availableUsersProfile = users.map((user): UserSkillsExpectations => userToHrResponse(user));
+    const availableUsersProfile = users
+      .map((user): UserSkillsExpectations => {
+        if (user.profile && user.profile._id !== null) {
+          return userToHrResponse(user);
+        }
+      })
+      .filter((user) => user);
 
     res.status(200).send(availableUsersProfile);
   } catch (err) {
@@ -63,13 +69,21 @@ export const reservedUsers = async (req: Request, res: Response, next: NextFunct
     if (req.user._id.toString() !== id) {
       throw new ValidationError('Token not match to user');
     }
+
     const hr = await UserDb.findById(id);
 
     const reservedUsers = await Promise.all(
       hr.users.map((email) => UserSkillDb.findOne({ email }).populate('profile'))
     );
 
-    const reservedUsersProfile = reservedUsers.map((user): UserSkillsExpectations => userToHrResponse(user));
+    const reservedUsersProfile = reservedUsers
+      .map((user): UserSkillsExpectations => {
+        if (user.profile && user.profile._id !== null) {
+          return userToHrResponse(user);
+        }
+      })
+      .filter((user) => user);
+
     res.status(200).send(reservedUsersProfile);
   } catch (err) {
     next(err);
@@ -84,15 +98,20 @@ export const setStatus = async (req: Request, res: Response, next: NextFunction)
       throw new ValidationError('Token not match to user');
     }
 
+    const hr = await UserDb.findById(id);
+    if (!hr) throw new ValidationError('Hr not found');
+
     const user = await UserDb.findOne({ email });
     if (!user) throw new ValidationError('Users not found');
+
+    const isUserInArray = hr.users.includes(email);
+    if (user.status.status === 'W trakcie rozmowy' && !isUserInArray) {
+      throw new ValidationError(`Kursant jest już umówiony na rozmowę z inną firmą`);
+    }
 
     if (user.status.status === status) throw new ValidationError(`Kursant jest już ${status}`);
     user.status.status = status;
     user.save();
-
-    const hr = await UserDb.findById(id);
-    const isUserInArray = hr.users.includes(email);
 
     if (status === 'W trakcie rozmowy' && !isUserInArray) {
       const notMaxReservedStudents = hr.maxReservedStudents > hr.users.length;
@@ -108,6 +127,55 @@ export const setStatus = async (req: Request, res: Response, next: NextFunction)
     }
 
     res.status(200).send(filterHr(hr));
+  } catch (err) {
+    next(err);
+  }
+};
+
+export const filterUsers = async (req: Request, res: Response, next: NextFunction) => {
+  const {
+    courseCompletion,
+    courseEngagement,
+    projectDegree,
+    teamProjectDegree,
+    expectedTypeWork,
+    expectedSalary,
+    canTakeApprenticeship,
+    monthsOfCommercialExp,
+  } = req.body;
+
+  try {
+    const filter = {
+      courseCompletion: { $gte: courseCompletion ?? 0 },
+      courseEngagement: { $gte: courseEngagement ?? 0 },
+      projectDegree: { $gte: projectDegree ?? 0 },
+      teamProjectDegree: { $gte: teamProjectDegree ?? 0 },
+      profile: { $exists: true },
+    };
+
+    const filterUsers = await UserSkillDb.find(filter).populate({
+      path: 'profile',
+      match: {
+        expectedTypeWork: expectedTypeWork ?? {
+          $in: ['Na miejscu', 'Gotowość do przeprowadzki', 'Wyłącznie zdalnie', 'Hybrydowo', 'Bez znaczenia'],
+        },
+        expectedSalary: { $gte: expectedSalary ?? '' },
+        canTakeApprenticeship: canTakeApprenticeship ?? {
+          $in: ['TAK', 'NIE'],
+        },
+        monthsOfCommercialExp: { $gte: monthsOfCommercialExp ?? 0 },
+      },
+    });
+
+    const filtered = filterUsers
+      .map((user): UserSkillsExpectations => {
+        if (user.profile && user.profile._id !== null) {
+          return userToHrResponse(user);
+        }
+      })
+      .filter((user) => user);
+
+    res.status(200).send(filtered);
   } catch (err) {
     next(err);
   }
--- routers/hr.router.ts
+++ routers/hr.router.ts
@@ -1,5 +1,5 @@
 import { Router } from 'express';
-import { availableUsers, reservedUsers, searchUsers, setStatus } from '../controllers/hrController';
+import { availableUsers, filterUsers, reservedUsers, searchUsers, setStatus } from '../controllers/hrController';
 import { verifyCookie } from '../middlewares/auth';
 import { ROLES, verifyRole } from '../middlewares/checkRole';
 import { validation } from '../middlewares/validation';
@@ -10,5 +10,6 @@ export const hrRouter = Router();
 hrRouter
   .get('/', verifyCookie, verifyRole(ROLES.hr), availableUsers)
   .get('/search', verifyCookie, verifyRole(ROLES.hr), searchUsers)
+  .get('/filter', verifyCookie, verifyRole(ROLES.hr), filterUsers)
   .get('/:id', verifyCookie, verifyRole(ROLES.hr), reservedUsers)
   .patch('/status/:id', verifyCookie, verifyRole(ROLES.hr), validation(setStatusSchema), setStatus);
