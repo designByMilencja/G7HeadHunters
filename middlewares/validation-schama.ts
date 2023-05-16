f9d15474d710db80fdec6a2b04ac4aae6bf7854c
s1avo <s.nadolny@hotmail.com>
2023-05-16 22:28:03 +0200

    ref(filter validation): added missing fields

--- middlewares/validation-schama.ts
+++ middlewares/validation-schama.ts
@@ -1,5 +1,7 @@
 import * as yup from 'yup';
 
+const WORKTYPE = ['Na miejscu', 'Gotowość do przeprowadzki', 'Wyłącznie zdalnie', 'Hybrydowo', 'Bez znaczenia'];
+const CONTRACTTYPE = ['Tylko UoP', 'Możliwe B2B', 'Możliwe UZ/UoD', 'Brak preferencji'];
 export const filterUsersSchema = yup.object().shape({
   courseCompletion: yup
     .number()
@@ -28,5 +30,16 @@ export const filterUsersSchema = yup.object().shape({
     .max(5, 'Maksymalna ocena z pracy w zespole przy projekcie bonusowym to 5')
     .integer('Tylko liczby naturalne w polu z oceny pracy w zespole przy projekcie bonusowym.')
     .typeError('Dozwolone są tylko liczny naturalne w polu z oceny pracy w zespole przy projekcie bonusowym.')
+    .notRequired(),
+  expectedTypeWork: yup.string().oneOf(WORKTYPE, 'Nie prawidłowe dane w polu oczekiwane miejsce pracy.').notRequired(),
+  expectedContractType: yup.string().oneOf(CONTRACTTYPE, 'Nie prawidłowe dane w polu typ kontraktu').notRequired(),
+  expectedSalaryFrom: yup.number().min(0, 'Minimalna wartość w polu oczekiwana wypłata to 0').notRequired(),
+  expectedSalaryTo: yup.number().min(0, 'Minimalna wartość w polu oczekiwana wypłata to 0').notRequired(),
+  canTakeApprenticeship: yup.string().oneOf(['TAK', 'NIE'], 'Wybierz prawidłowe dane: "TAK" lub "NIE"').notRequired(),
+  monthsOfCommercialExp: yup
+    .number()
+    .min(0, 'Minimalna liczba miesięcy doświadczenia komercyjnego to 0')
+    .integer('Tylko liczby naturalne w polu ilość miesięcy doświadczenia komercyjnego.')
+    .typeError('Dozwolone są tylko liczny naturalne w polu ilość miesięcy doświadczenia komercyjnego')
     .notRequired(),
 });
