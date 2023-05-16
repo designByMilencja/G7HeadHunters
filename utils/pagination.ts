efc5c216e7b546947f4c2da92a62c89416218435
s1avo <s.nadolny@hotmail.com>
2023-05-16 22:32:12 +0200

    ref(utils): pagination with aggregate

--- utils/pagination.ts
+++ utils/pagination.ts
@@ -1,28 +1,44 @@
-import { Document, Query } from 'mongoose';
-import { IUserSkillsDocument } from '../models/UserSkillsSchema';
+import { PipelineStage } from 'mongoose';
+import { IUserSkillsDocument, UserSkillDb } from '../models/UserSkillsSchema';
+import { IUserProfileDocument } from '../models/UserProfileSchema';
+
+type UserSkillsExpectations = IUserSkillsDocument & { profile: IUserProfileDocument };
+type Result = [results: UserSkillsExpectations[], totalPage: { count: number }[]];
 
 const limits = [10, 25, 50];
 const defaultLimit = 10;
-export const pagination = async (
-  query: Query<Document<IUserSkillsDocument>[], Document<IUserSkillsDocument>, {}, IUserSkillsDocument>,
-  page: number,
-  limit: number
-) => {
+
+export const pipeline = (filter: any) => {
+  return [
+    {
+      $lookup: {
+        from: 'userprofiles',
+        localField: 'profile',
+        foreignField: '_id',
+        as: 'profile',
+      },
+    },
+    {
+      $unwind: '$profile',
+    },
+    {
+      $match: filter,
+    },
+  ];
+};
+
+export const pagination = async (pipeline: PipelineStage[], page: number, limit: number) => {
   limit = limits.includes(limit) ? limit : defaultLimit;
 
-  const [results, total] = await Promise.all([
-    query
-      .populate({
-        path: 'profile',
-      })
+  const [results, total] = (await Promise.all([
+    UserSkillDb.aggregate(pipeline)
       .skip((page - 1) * limit)
       .limit(limit)
-      .lean()
       .exec(),
-    query.model.countDocuments(query.getFilter()),
-  ]);
+    UserSkillDb.aggregate([...pipeline, { $count: 'count' }]).exec(),
+  ])) as Result;
 
-  const totalPages = Math.ceil(total / limit);
+  const totalPages = Math.ceil(total[0].count / limit);
 
   return { results, totalPages };
 };
